3d487474d4281d99cc361f5ddfbe94a97485fe2d
Havock <122547813+HavocGrapeSoda@users.noreply.github.com>
2026-02-20 20:00:49 +0800

    switched calorie counting to using weight

--- app/src/main/java/com/nutriscan/ui/caloriesburned/CaloriesBurnedViewModel.kt
+++ app/src/main/java/com/nutriscan/ui/caloriesburned/CaloriesBurnedViewModel.kt
@@ -47,9 +47,17 @@ class CaloriesBurnedViewModel @Inject constructor(
     /** Whether tracking is active. */
     val isTrackingActive: StateFlow<Boolean> = StepCounterService.isServiceRunning
 
-    /** Calories burned today from DB. */
-    val caloriesBurned: StateFlow<Double> = liveSteps.map { steps ->
-        steps * 0.04
+    val userWeight: StateFlow<Int> = mealRepository.getWeight()
+        .stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), 70) // 70 as default weight
+
+    /** Calories burnt from physical activity*/
+    val caloriesBurned: StateFlow<Double> = combine(liveSteps, userWeight) { steps, weight ->
+        val multiplier = when {
+            weight >= 86 -> 0.55
+            weight >= 70 -> 0.45
+            else -> 0.35
+        }
+        steps * multiplier
     }.stateIn(viewModelScope, SharingStarted.WhileSubscribed(5000), 0.0)
 
     /** Calories from food*/
